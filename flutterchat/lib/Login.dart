e38993e07ff65d5764d4ba1d96d11c852309a46c
Hyuntae H <simp1eLogic@users.noreply.github.com>
2022-12-05 21:46:06 +0900

    .

--- flutterchat/lib/Login.dart
+++ flutterchat/lib/Login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutterchat/chat_screen.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
